489e319d88d31f141541c2f30c91e975dc29927b
Fellipe Henry Bordon <fehb21@gmail.com>
2025-02-14 18:58:57 -0300

    Adjusting exception handler

--- src/main/java/com/fhbhub/javaspringapi/exception/ExceptionResponse.java
+++ src/main/java/com/fhbhub/javaspringapi/exception/ExceptionResponse.java
@@ -1,21 +1,5 @@
 package com.fhbhub.javaspringapi.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import java.io.Serial;
-import java.io.Serializable;
 import java.util.Date;
 
-@AllArgsConstructor
-@Getter
-public class ExceptionResponse implements Serializable {
-
-	@Serial
-	private static final long serialVersionUID = 1L;
-	
-	private Date timestamp;
-	private String message;
-	private String details;
-
-}
+public record ExceptionResponse(Date timestamp, String message, String details) {}
